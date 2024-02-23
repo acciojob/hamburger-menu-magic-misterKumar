211b500b439a2d238c5056733b1365f35864a43d
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-02-23 18:56:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const drawer = document.createElement('div');
+drawer.classList.add('links-drawer-container');
+drawer.append(document.querySelector('.navbar-links-list').cloneNode(true));
+
+document.querySelector('nav').appendChild(drawer);
+
+const menuButton = document.querySelector('.menu-toggle-button');
+
+menuButton.addEventListener('click', (event) => {
+    menuButton.classList.toggle('open-menu');
+    drawer.classList.toggle('slide-in');
+
+    menuButton.classList.toggle('close-menu');
+})
